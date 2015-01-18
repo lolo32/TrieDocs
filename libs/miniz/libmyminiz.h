ac6725be965e98f7cf39b3d95ae08727d022e38c
Laurent Baysse (Lolo_32) <lol.b@free.fr>
2015-01-18 19:01:08 +0100

    Utilisation du fichiers source de mini pour les déclarations
    Signed-off-by: Laurent Baysse (Lolo_32) <lol.b@free.fr>

--- libs/miniz/libmyminiz.h
+++ libs/miniz/libmyminiz.h
@@ -12,29 +12,8 @@
 
 #ifndef MZ_LIBMINIZ
 
-#if 0
-// -----------------------------
-// Définitions dans miniz.c à appeler depuis l'extérieur
-
-// For more compatibility with zlib, miniz.c uses unsigned long for some parameters/struct members. Beware: mz_ulong can be either 32 or 64-bits!
-typedef unsigned long mz_ulong;
-
-// Returns the version string of miniz.c.
-const char *mz_version(void);
-
-// Single-call compression functions mz_compress2():
-// Returns MZ_OK on success, or one of the error codes from mz_deflate() on failure.
-int mz_compress2(unsigned char *pDest, mz_ulong *pDest_len, const unsigned char *pSource, mz_ulong source_len, int level);
-
-// Single-call decompression.
-// Returns MZ_OK on success, or one of the error codes from mz_inflate() on failure.
-int mz_uncompress(unsigned char *pDest, mz_ulong *pDest_len, const unsigned char *pSource, mz_ulong source_len);
-#else
-
 #define MINIZ_HEADER_FILE_ONLY
-#include "miniz.c"
-
-#endif
+#include "../../3party/miniz-r63/miniz.c"
 
 #endif
 
