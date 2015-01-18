a4c4a3b77ab8dc9ce509823760112140eb1e349a
Laurent Baysse (Lolo_32) <lol.b@free.fr>
2015-01-18 19:40:15 +0100

    Ajout de la traduction en anglais au programme
    Signed-off-by: Laurent Baysse (Lolo_32) <lol.b@free.fr>

---
+++ src/i18n/en.ts
@@ -0,0 +1,200 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en" sourcelanguage="fr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>TrieDocs</source>
+        <translation></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../mainwindow.ui" line="30"/>
+        <source>Étiquettes</source>
+        <translation>Tags</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="52"/>
+        <source>Rechercher</source>
+        <translation>Search</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="75"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="103"/>
+        <source>&amp;Fichier</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="111"/>
+        <source>&amp;Options</source>
+        <translation>&amp;Options</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="119"/>
+        <source>&amp;?</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="130"/>
+        <source>&amp;Connexion...</source>
+        <translation>&amp;Connection...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="135"/>
+        <source>&amp;Quitter</source>
+        <translation>E&amp;xit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="140"/>
+        <source>&amp;Configuration...</source>
+        <translation>&amp;Config...</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../mainwindow.ui" line="145"/>
+        <source>À &amp;propos de...</source>
+        <translation>&amp;About...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="150"/>
+        <source>&amp;Importer des fichiers...</source>
+        <translation>&amp;Import files...</translation>
+    </message>
+</context>
+<context>
+    <name>sqlite_msgerr</name>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="157"/>
+        <source>erreur de logique SQL ou base de données manquante</source>
+        <translation>SQL logic error or missing database</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="159"/>
+        <source>accès refusé</source>
+        <translation>access permission denied</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="160"/>
+        <source>la fonction de retours a demandé l&apos;annulation de la requète</source>
+        <translation>callback requested query abort</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="161"/>
+        <source>base de données verrouillée</source>
+        <translation>database is locked</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="162"/>
+        <source>table verrouillée</source>
+        <translation>database table is locked</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="163"/>
+        <source>mémoire insuffisante</source>
+        <translation>out of memory</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="164"/>
+        <source>tentative d&apos;écriture dans une base de données en lecture seule</source>
+        <translation>attempt to write a readonly database</translation>
+    </message>
+    <message>
+        <location filename="../bdd.cpp" line="165"/>
+        <source>interrompu</source>
+        <translation>interrupted</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="166"/>
+        <source>erreur d&apos;entrée/sortie du disque</source>
+        <translation>disk I/O error</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="167"/>
+        <source>la base de donnée est corrompue</source>
+        <translation>database disk image is malformed</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="168"/>
+        <source>opération inconnue</source>
+        <translation>unknown operation</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="169"/>
+        <source>base de données ou disque plein</source>
+        <translation>database or disk is full</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="170"/>
+        <source>impossible d&apos;ouvrir le fichier de la base de données</source>
+        <translation>unable to open database file</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="171"/>
+        <source>protocole bloqué</source>
+        <translation>locking protocol</translation>
+    </message>
+    <message>
+        <location filename="../bdd.cpp" line="172"/>
+        <source>la table est vide</source>
+        <translation>table contains no data</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="173"/>
+        <source>le schéma de la base de données a changé</source>
+        <translation>database schema has changed</translation>
+    </message>
+    <message>
+        <location filename="../bdd.cpp" line="174"/>
+        <source>texte ou binaire trop long</source>
+        <translation>string or blob too big</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="175"/>
+        <source>échec de contrainte</source>
+        <translation>constraint failed</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="176"/>
+        <source>type de donnée incorrect</source>
+        <translation>datatype mismatch</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="177"/>
+        <source>routine de bibliothèque appelé hors de la séquence</source>
+        <translation>library routine called out of sequence</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="178"/>
+        <source>support des grands fichiers désactivé</source>
+        <translation>large file support is disabled</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="179"/>
+        <source>autorisation refusée</source>
+        <translation>authorization denied</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="180"/>
+        <source>erreur de format de base de données auxiliaire</source>
+        <translation>auxiliary database format error</translation>
+    </message>
+    <message>
+        <location filename="../bdd.cpp" line="181"/>
+        <source>bind ou index de colonne hors de la plage</source>
+        <translation>bind or column index out of range</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../bdd.cpp" line="182"/>
+        <source>le fichier est chiffré ou n&apos;est pas une base de données</source>
+        <translation>file is encrypted or is not a database</translation>
+    </message>
+    <message>
+        <location filename="../bdd.cpp" line="184"/>
+        <source>erreur inconnue</source>
+        <translation>unknown error</translation>
+    </message>
+</context>
+</TS>
