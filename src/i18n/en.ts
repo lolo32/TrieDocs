d2a00cc93ed5f5eaaa490198595173195bb6797d
Laurent Baysse (Lolo_32) <lol.b@free.fr>
2015-01-19 19:49:15 +0100

    Mise à jour de la traduction anglaise
    Signed-off-by: Laurent Baysse (Lolo_32) <lol.b@free.fr>

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -1,6 +1,35 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="en" sourcelanguage="fr">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>DlgClient</name>
+    <message>
+        <location filename="../dlgclient.ui" line="26"/>
+        <source>Veuillez vous identifier pour avoir accès aux documents</source>
+        <translation>Please, login to access documents</translation>
+    </message>
+    <message>
+        <location filename="../dlgclient.ui" line="44"/>
+        <source>Identifiant de connexion</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../dlgclient.ui" line="57"/>
+        <source>Mot de passe</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <location filename="../dlgclient.ui" line="68"/>
+        <source>Langue :</source>
+        <translation>Language:</translation>
+    </message>
+    <message>
+        <location filename="../dlgclient.cpp" line="41"/>
+        <source>Établissement de la connexion avec le serveur %1</source>
+        <translation>Connecting to server %1</translation>
+    </message>
+</context>
 <context>
     <name>MainWindow</name>
     <message>
@@ -8,7 +37,7 @@
         <source>TrieDocs</source>
         <translation></translation>
     </message>
-    <message utf8="true">
+    <message>
         <location filename="../mainwindow.ui" line="30"/>
         <source>Étiquettes</source>
         <translation>Tags</translation>
@@ -53,7 +82,7 @@
         <source>&amp;Configuration...</source>
         <translation>&amp;Config...</translation>
     </message>
-    <message utf8="true">
+    <message>
         <location filename="../mainwindow.ui" line="145"/>
         <source>À &amp;propos de...</source>
         <translation>&amp;About...</translation>
@@ -66,133 +95,133 @@
 </context>
 <context>
     <name>sqlite_msgerr</name>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="157"/>
+    <message>
+        <location filename="../bdd.cpp" line="187"/>
         <source>erreur de logique SQL ou base de données manquante</source>
         <translation>SQL logic error or missing database</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="159"/>
+    <message>
+        <location filename="../bdd.cpp" line="189"/>
         <source>accès refusé</source>
         <translation>access permission denied</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="160"/>
+    <message>
+        <location filename="../bdd.cpp" line="190"/>
         <source>la fonction de retours a demandé l&apos;annulation de la requète</source>
         <translation>callback requested query abort</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="161"/>
+    <message>
+        <location filename="../bdd.cpp" line="191"/>
         <source>base de données verrouillée</source>
         <translation>database is locked</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="162"/>
+    <message>
+        <location filename="../bdd.cpp" line="192"/>
         <source>table verrouillée</source>
         <translation>database table is locked</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="163"/>
+    <message>
+        <location filename="../bdd.cpp" line="193"/>
         <source>mémoire insuffisante</source>
         <translation>out of memory</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="164"/>
+    <message>
+        <location filename="../bdd.cpp" line="194"/>
         <source>tentative d&apos;écriture dans une base de données en lecture seule</source>
         <translation>attempt to write a readonly database</translation>
     </message>
     <message>
-        <location filename="../bdd.cpp" line="165"/>
+        <location filename="../bdd.cpp" line="195"/>
         <source>interrompu</source>
         <translation>interrupted</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="166"/>
+    <message>
+        <location filename="../bdd.cpp" line="196"/>
         <source>erreur d&apos;entrée/sortie du disque</source>
         <translation>disk I/O error</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="167"/>
+    <message>
+        <location filename="../bdd.cpp" line="197"/>
         <source>la base de donnée est corrompue</source>
         <translation>database disk image is malformed</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="168"/>
+    <message>
+        <location filename="../bdd.cpp" line="198"/>
         <source>opération inconnue</source>
         <translation>unknown operation</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="169"/>
+    <message>
+        <location filename="../bdd.cpp" line="199"/>
         <source>base de données ou disque plein</source>
         <translation>database or disk is full</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="170"/>
+    <message>
+        <location filename="../bdd.cpp" line="200"/>
         <source>impossible d&apos;ouvrir le fichier de la base de données</source>
         <translation>unable to open database file</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="171"/>
+    <message>
+        <location filename="../bdd.cpp" line="201"/>
         <source>protocole bloqué</source>
         <translation>locking protocol</translation>
     </message>
     <message>
-        <location filename="../bdd.cpp" line="172"/>
+        <location filename="../bdd.cpp" line="202"/>
         <source>la table est vide</source>
         <translation>table contains no data</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="173"/>
+    <message>
+        <location filename="../bdd.cpp" line="203"/>
         <source>le schéma de la base de données a changé</source>
         <translation>database schema has changed</translation>
     </message>
     <message>
-        <location filename="../bdd.cpp" line="174"/>
+        <location filename="../bdd.cpp" line="204"/>
         <source>texte ou binaire trop long</source>
         <translation>string or blob too big</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="175"/>
+    <message>
+        <location filename="../bdd.cpp" line="205"/>
         <source>échec de contrainte</source>
         <translation>constraint failed</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="176"/>
+    <message>
+        <location filename="../bdd.cpp" line="206"/>
         <source>type de donnée incorrect</source>
         <translation>datatype mismatch</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="177"/>
+    <message>
+        <location filename="../bdd.cpp" line="207"/>
         <source>routine de bibliothèque appelé hors de la séquence</source>
         <translation>library routine called out of sequence</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="178"/>
+    <message>
+        <location filename="../bdd.cpp" line="208"/>
         <source>support des grands fichiers désactivé</source>
         <translation>large file support is disabled</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="179"/>
+    <message>
+        <location filename="../bdd.cpp" line="209"/>
         <source>autorisation refusée</source>
         <translation>authorization denied</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="180"/>
+    <message>
+        <location filename="../bdd.cpp" line="210"/>
         <source>erreur de format de base de données auxiliaire</source>
         <translation>auxiliary database format error</translation>
     </message>
     <message>
-        <location filename="../bdd.cpp" line="181"/>
+        <location filename="../bdd.cpp" line="211"/>
         <source>bind ou index de colonne hors de la plage</source>
         <translation>bind or column index out of range</translation>
     </message>
-    <message utf8="true">
-        <location filename="../bdd.cpp" line="182"/>
+    <message>
+        <location filename="../bdd.cpp" line="212"/>
         <source>le fichier est chiffré ou n&apos;est pas une base de données</source>
         <translation>file is encrypted or is not a database</translation>
     </message>
     <message>
-        <location filename="../bdd.cpp" line="184"/>
+        <location filename="../bdd.cpp" line="214"/>
         <source>erreur inconnue</source>
         <translation>unknown error</translation>
     </message>
