48e5b163a42fdd1d4c7ae7d02a9b543c1cbfa5ad
Laurent B. (Lolo_32) <lol.b@free.fr>
2015-02-04 22:30:37 +0100

    Utilisation de la version du projet pour tous les numéros de version
    Signed-off-by: Laurent B. (Lolo_32) <lol.b@free.fr>

--- src/cmake/MacOS.cmake
+++ src/cmake/MacOS.cmake
@@ -1,5 +1,5 @@
 # Options pour MacOSx
-SET( MACOSX_BUNDLE_INFO_STRING "${PROJECT_NAME}, ${VERSION_MAJEUR}.${VERSION_MINEUR}.${VERSION_REVISION} — 2015 — Laurent B." )
+SET( MACOSX_BUNDLE_INFO_STRING "${PROJECT_NAME}, ${TrieDocs_VERSION_MAJOR}.${TrieDocs_VERSION_MINOR}.${TrieDocs_VERSION_PATCH} — 2015 — Laurent B." )
 
 SET( TrieDocs_ICONE "${CMAKE_CURRENT_SOURCE_DIR}/icone.icns" )
 
@@ -7,11 +7,11 @@ SET( MACOSX_BUNDLE_ICON_FILE icone.icns )
 # set where in the bundle to put the icns file
 SET_SOURCE_FILES_PROPERTIES( ${TrieDocs_ICONE} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources" )
 
-SET( MACOSX_BUNDLE_GUI_IDENTIFIER       "fr.baysse.${PROJECT_NAME}"                                                        )
-SET( MACOSX_BUNDLE_LONG_VERSION_STRING  "${PROJECT_NAME} version ${VERSION_MAJEUR}.${VERSION_MINEUR}.${VERSION_REVISION}"  )
-SET( MACOSX_BUNDLE_BUNDLE_NAME          "${PROJECT_NAME}"                                                                  )
-SET( MACOSX_BUNDLE_SHORT_VERSION_STRING "${VERSION_MAJEUR}.${VERSION_MINEUR}.${VERSION_REVISION}"                          )
-SET( MACOSX_BUNDLE_BUNDLE_VERSION       "${VERSION_MAJEUR}.${VERSION_MINEUR}.${VERSION_REVISION}"                          )
-SET( MACOSX_BUNDLE_COPYRIGHT            "Laurent B. — Tous droits réservés © 2015"                                         )
+SET( MACOSX_BUNDLE_GUI_IDENTIFIER       "fr.baysse.${PROJECT_NAME}"                                                       )
+SET( MACOSX_BUNDLE_LONG_VERSION_STRING  "${PROJECT_NAME} version ${TrieDocs_VERSION_MAJOR}.${TrieDocs_VERSION_MINOR}.${TrieDocs_VERSION_PATCH}"  )
+SET( MACOSX_BUNDLE_BUNDLE_NAME          "${PROJECT_NAME}"                                                                 )
+SET( MACOSX_BUNDLE_SHORT_VERSION_STRING "${TrieDocs_VERSION_MAJOR}.${TrieDocs_VERSION_MINOR}.${TrieDocs_VERSION_PATCH}"   )
+SET( MACOSX_BUNDLE_BUNDLE_VERSION       "${TrieDocs_VERSION_MAJOR}.${TrieDocs_VERSION_MINOR}.${TrieDocs_VERSION_PATCH}"   )
+SET( MACOSX_BUNDLE_COPYRIGHT            "Laurent B. — All rights reserved © 2015"                                         )
 
 SET( APPS "\${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}.app" )
